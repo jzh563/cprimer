febb9503a1c9ce3cf3331adabc5ab0563a144cc4
jzh563 <1220826949@qq.com>
2018-08-06 14:49:26 +0800

    chapter 4 4.3code

--- ch04/praise2.c
+++ ch04/praise2.c
@@ -3,8 +3,14 @@
 #define PRAISE "what a super marvelous name !"
 int main(void)
 {
-	chae name[40];
+	char name[40];
 	printf("What's your name?\n");
-	scanf("%s", name);
+	scanf_s("%s", name,40);
 	printf("Hello,%s,%s\n", name, PRAISE);
-	printf("Your name of %d  lettersoccupies %d memory cells ");
+	printf("Your name of %d  letters occupies %d memory cells.\n", strlen(name), sizeof name);
+	printf("The phrase of praise has %d letters ", strlen(PRAISE));
+	printf("and occupies %d memory cells.\n", sizeof PRAISE);
+	getchar();
+	getchar();
+	return 0;
+}
